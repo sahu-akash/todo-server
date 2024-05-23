cc813d6a3d75ab252a3c0f97c0d076117eeb53de
Akash Sahu <68236849+sahu-akash@users.noreply.github.com>
2024-05-23 11:33:49 +0300

    Update test.js

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ const option2 = process.env.OPTION2;
 
 console.log(`Option 1 selected: ${option1}`);
 console.log(`Option 2 selected: ${option2}`);
+console.log(`First Key from secrets: ${firstKey}`);
 
 // Your application logic here
 // For example, you could call different functions based on the selected options
